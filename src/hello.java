b945e2e81e30bb12099d9a8352e1982548ab15a0
ashwininilakh28 <nilakhashwini28@gmail.com>
2019-03-30 15:18:01 +0530

    Dev 2 changes

--- src/hello.java
+++ src/hello.java
@@ -4,6 +4,7 @@ public class hello {
 	public static void main(String[] args) {
 		System.out.println("Hello World");  //syso control+space
 System.out.println("my first commit");
+System.out.println("Dev2 Changes");
 	}
 
 }
