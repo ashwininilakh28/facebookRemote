56403a36dca8aa4c395621e0317a7d1588e23fa3
ashwininilakh28 <nilakhashwini28@gmail.com>
2019-03-30 15:59:55 +0530

    changes by branch

--- src/hello.java
+++ src/hello.java
@@ -5,7 +5,10 @@ public class hello {
 		System.out.println("Hello World");  //syso control+space
 System.out.println("my first commit");
 System.out.println("Dev2 Changes");
-	System.out.println("chnages through eclipse");}
+	System.out.println("chnages through eclipse");
+	System.out.println("chnages by eclipse");}
 	
+// branch changes
 
+	
 }
